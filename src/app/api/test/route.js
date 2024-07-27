a8b5fbfd7549d729a4cf93341d3d850965c62ec6
pudhi99 <prudhvi.inumarthi99@gmail.com>
2024-07-27 11:32:31 +0530

    removed test api

--- src/app/api/test/route.js
+++
@@ -1,8 +0,0 @@
-import mongo from "@/utils/db";
-import { NextResponse } from "next/server";
-
-export async function GET(request) {
-  const db = await mongo();
-  const test = await db.collection("test").find({}).toArray();
-  return NextResponse.json(test);
-}
